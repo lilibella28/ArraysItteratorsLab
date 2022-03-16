42d297a22b42ccdba43fcdf90b4609fe06f50107
lilibella28 <65881308+lilibella28@users.noreply.github.com>
2022-03-16 15:44:03 -0700

    Add files via upload

---
+++ JS/app.js
@@ -0,0 +1,222 @@
+const inventors = [
+    { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
+    { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
+    { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
+    { first: 'Marie', last: 'Curie', year: 1867, passed: 1934 },
+    { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 },
+    { first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 },
+    { first: 'Max', last: 'Planck', year: 1858, passed: 1947 },
+    { first: 'Katherine', last: 'Blodgett', year: 1898, passed: 1979 },
+    { first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852 },
+    { first: 'Sarah E.', last: 'Goode', year: 1855, passed: 1905 },
+    { first: 'Lise', last: 'Meitner', year: 1878, passed: 1968 },
+    { first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 }
+  ];
+
+    
+  /*
+  
+  1. Filter the array of inventors into a new array 
+  containing only the inventors born in the 1500's
+
+      
+  */
+
+  const investorsYear = inventors.filter(function(yearBorn) {
+       return yearBorn.year > 1500 && yearBorn.year < 1600;
+  })
+  console.log(`Question 1 testing `)
+  console.log(investorsYear)
+
+
+  /*
+  
+  // Array.prototype.map()
+// 2. Map the array of the inventors into
+ a new array containing objects with just 
+ the first and last names as properties
+
+
+  
+  */
+
+  const userInfo = inventors.map(function(name){
+      return {Name: name.first,  lastName: name.last}
+  })
+  console.log(`Question 2 testing `)
+  console.log(userInfo)
+
+
+ /*
+ / Array.prototype.sort()
+// 3. Sort the inventors by birthdate, oldest to youngest
+ 
+ */
+const bornDay = inventors.map(function(birthDay){
+    // console.log(birthDay.year)
+    return birthDay.year
+})
+console.log(`Question 3 testing `)
+console.log(bornDay.sort())
+
+/*
+
+
+// 4. Sort the inventors by years 
+lived from shortest to longest lived
+
+ */
+
+const passingYear = inventors.map(function(birthDay){
+    // console.log(birthDay.year)
+    return birthDay.passed;
+})
+
+console.log(`Question 4 testing `)
+console.log(passingYear)
+console.log(passingYear.sort())
+
+// Array.prototype.reduce()
+// 5. How many years did all the inventors live?
+
+const yearTheyLive = inventors.reduce(function(acc, num){
+        //  console.log(num.passed)
+        //  console.log(num.year)
+         acc = num.passed - num.year; 
+         return acc
+
+}, 0 )
+
+console.log(`Question 5 testing `)
+
+console.log(yearTheyLive)
+
+
+
+
+const people = [
+    'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick', 'Beecher, Henry',
+    'Beethoven, Ludwig', 'Begin, Menachem', 'Belloc, Hilaire', 'Bellow, Saul',
+    'Benchley, Robert', 'Benenson, Peter', 'Ben-Gurion, David',
+    'Benjamin, Walter', 'Benn, Tony', 'Bennington, Chester', 'Benson, Leana',
+    'Bent, Silas', 'Bentsen, Lloyd', 'Berger, Ric', 'Bergman, Ingmar',
+    'Berio, Luciano', 'Berle, Milton', 'Berlin, Irving', 'Berne, Eric',
+    'Bernhard, Sandra', 'Berra, Yogi', 'Berry, Halle', 'Berry, Wendell',
+    'Bethea, Erin', 'Bevan, Aneurin', 'Bevel, Ken', 'Biden, Joseph',
+    'Bierce, Ambrose', 'Biko, Steve', 'Billings, Josh', 'Biondo, Frank',
+    'Birrell, Augustine', 'Black, Elk', 'Blair, Robert', 'Blair, Tony',
+    'Blake, William'
+  ];
+  /*
+  
+// Array.prototype.map()
+// 6. Map the people array such that the new array consists of strings
+ with the names formatted as "First Last", e.g., "Becker, Carl" should 
+ be mapped to "Carl Becker".
+
+
+  
+  */
+
+  const stringsMaps = people.map(function(person){
+
+          return person.split(" ").reverse().join()
+          ///Omg Lili come back and read these
+  })
+
+  console.log(`Question 6 testing `)
+  console.log(stringsMaps)
+
+
+
+  const data = [
+    'car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van',
+    'bike', 'walk', 'car', 'van', 'car', 'truck'
+  ];
+
+  /*
+
+  Array.prototype.reduce()
+// 7. Count the number of instances for each of 
+the data items.
+// Hint:  Return an object where the keys are 
+'car', 'truck', etc. and the values are the count.
+
+   */
+  
+
+  const countingInstaOfdata = data.reduce(function(acc, num){
+
+        acc[num] = acc[num] ? acc[num] + 1 : 1;
+         return acc
+  }, {})//return and object
+
+  console.log(`Question 7 testing `)
+  console.log(countingInstaOfdata)
+
+  //===========Asked for help
+
+
+  const devs = [
+    { name: 'Wes', year: 1988 },
+    { name: 'Kait', year: 1986 },
+    { name: 'Irv', year: 1970 },
+    { name: 'Lux', year: 2015 }
+  ];
+
+  // Array.prototype.some()
+// 8. Check if at least one person is 19 or older // Array.prototype.some()
+// 8. Check if at least one person is 19 or older?
+
+
+let greatesThan19 = devs.some(function(age) {
+    // console.log(age.year)
+    return  2022 - age.year > 19;
+  });
+  console.log(`Question 8 testing `)
+  console.log(greatesThan19)
+
+
+
+// Array.prototype.every()
+// 9. Check if everyone is 19 or older?
+
+
+
+  let greatesThan19Every = devs.every(function(age) {
+    // console.log(age.year)
+    return  2022 - age.year > 19;
+  });
+
+  console.log(`Question 9 testing `)
+  console.log(greatesThan19Every)
+
+
+
+  const comments = [
+    { text: 'Love this!', id: 523423 },
+    { text: 'Super good', id: 823423 },
+    { text: 'You are the best', id: 2039842 },
+    { text: 'Ramen is my fav food ever', id: 123523 },
+    { text: 'Nice Nice Nice!', id: 542328 }
+  ];
+  
+//   // Array.prototype.find()
+// 10. Find the comment with the id of 823423
+
+
+const finds = comments.find(function(idnum){
+    return idnum.id === 823423
+})
+console.log(`Question 10 testing `)
+console.log(finds)
+
+// Array.prototype.findIndex()
+// 11. Find the index of the comment with an id of 123523
+
+const findsIndsx = comments.findIndex(function(idnum){
+    return idnum.id === 123523
+})
+
+console.log(`Question 10 testing `)
+console.log(findsIndsx)
